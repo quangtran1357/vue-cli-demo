f2a9f03ccfee90f6334b152726e4092b4b7a5512
quang.nt <quang.nt@hikosolution.com>
2021-11-04 21:48:46 +0700

    add_demo_pros_emit_event

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-<div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <p>----------------------------------------</p>=
-  <!-- <hello-world /> -->
-  <teacher-item />
-  <TeacherItem />
+  <div class="container">
+    <friend-contact v-for="teacher in teachers"
+    :id="teacher.id" 
+    :name="teacher.name" 
+    :phone="teacher.phone" 
+    :email="teacher.email"
+    :isFavorite="teacher.isFavorite"
+    :key="teacher.id"
+    @eventToggleFavorite="handleEventToggleFavorite"
+    @eventToggleDetail="handleEventToggleDetail"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TeacherItem from './components/teachers/item.vue'
-
+import FriendContact from './components/FriendContact.vue'
 export default {
+  components: { FriendContact },
   name: 'App',
-  components: {
-    HelloWorld,
-    TeacherItem
-  },
   data() {
     return {
-      demo: ''
+      teachers: [
+        {
+          id: 1,
+          name: 'Quang',
+          phone: 123123123,
+          email: 'quang@gmail.com',
+          isFavorite: false
+        },
+        {
+          id: 2,
+          name: 'Lan',
+          phone: 321321321,
+          email: 'lan@gmail.com',
+          isFavorite: false
+        }
+      ]
+    }
+  },
+  methods: {
+    handleEventToggleFavorite(id) {
+      // Tất cả các cách đều được
+      const teacherIndex = this.teachers.findIndex(item => item.id === id) //index
+      if (teacherIndex >= 0) {
+        const teacher = this.teachers[teacherIndex]
+        teacher.isFavorite = !teacher.isFavorite
+        // this.teachers[teacherIndex].isFavorite = !teacher.isFavorite
+        
+      }
+
+      // const teacher = this.teachers.find(item => item.id === id)
+      // if (teacher) {
+      //   teacher.isFavorite = !teacher.isFavorite
+      // }
+    },
+    handleEventToggleDetail(id) {
+      console.log(id)
     }
   }
 }
@@ -34,5 +69,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
 </style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="friend-contact" :class="{'is-favorite': isFavorite}">
+    <h2>{{ name }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetail">Show Details</button>
+    <ul>
+        <li>Phone: {{phone}}</li>
+        <li>Email: {{email}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FriendContact',
+//   props: ['name', 'phone', 'email'],
+  props: {
+    id: {
+        type: Number,
+        required: true
+    },
+    name: String, // 10,
+    phone: {
+        type: Number, //Number Array Boolean Object Date Function Symbol
+        required: true //Default: false
+    },
+    email: {
+        type: String,
+        default: 'lan123@gmail.com'  //String, Number, Boolean. Data ex: '', 10, true,
+                                  //Array, Object. Data ex: function () { return []; }, () => {}
+    },
+    color: {
+        type: Object,
+        default: () => {}
+    },
+    listStudent: {
+        type: Array,
+        default: () => []
+    },
+    isFavorite: {
+        type: Boolean,
+        dafault: false
+    }
+  },
+//   emits: ['eventToggleFavorite', 'eventToggleDetail'], // define custom events (),
+  emits: {
+      eventToggleFavorite: (id) => {
+          console.log(id)
+          if (id > 1) {
+              return true
+          } else {
+              console.log('ID đang bé hơn hoăc = 1')
+              return false
+          }
+      }
+  },
+  data() {
+      return {
+          demoData: ''
+      }
+  },
+  methods: {
+      toggleFavorite() {
+        this.$emit('eventToggleFavorite', this.id)
+      },
+      toggleDetail() {
+        this.$emit('eventToggleDetail', this.id)
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.is-favorite {
+    background-color: wheat
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+button:hover,
+button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
